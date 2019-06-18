8f6d5c8c98973f5db8d696aa937120237b7e8fea
sujitrj17 <34299336+sujitrj17@users.noreply.github.com>
2019-06-18 15:39:57 +0530

    Update OnetoOneCrudRepositoryApplicationTests.java
    m1 created

--- src/test/java/com/cjc/OnetoOneCrudRepositoryApplicationTests.java
+++ src/test/java/com/cjc/OnetoOneCrudRepositoryApplicationTests.java
@@ -8,9 +8,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OnetoOneCrudRepositoryApplicationTests {
-
+// changed 
 	@Test
 	public void contextLoads() {
+		
+		
+		
+	}
+	
+	public void m1() {
+		
+		
+		
 	}
 
 }
